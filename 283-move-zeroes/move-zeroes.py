9603e3e4ad9af2419e5519cbd69252ec69c79463
Carmine Choi <carmine.choi@gmail.com>
2023-08-28 17:19:20 -0700

    Time: 139 ms (92.11%) | Memory: 17.9 MB (84.17%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -11,18 +11,36 @@ class Solution:
         #     else:
         #         j += 1
 
-        i = j = 0
+        # i = j = 0
         
-        while i < len(nums):
-            while j < len(nums) and nums[j] == 0: 
-                j += 1
+        # while i < len(nums):
+        #     while j < len(nums) and nums[j] == 0: 
+        #         j += 1
 
-            if j >= len(nums):
-                break
+        #     if j >= len(nums):
+        #         break
             
-            if nums[i] == 0:
-                nums[i], nums[j] = nums[j], nums[i]
+        #     if nums[i] == 0:
+        #         nums[i], nums[j] = nums[j], nums[i]
 
-            i += 1
-            j += 1
+        #     i += 1
+        #     j += 1
+
+        index = 0  
+       
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                # If the current element is non-zero, swap it with the element at index
+                # This effectively moves non-zero elements to the beginning of the list
+                nums[i], nums[index] = nums[index], nums[i]
+                index += 1 
+
+        
+        # 0 1 0 3 12
+        # 1 0 0 3 12
+        # 1 3 0 0 12
+        # 1 3 12 0 0
+
+        # time: O(n)
+        # space: O(1)
 
