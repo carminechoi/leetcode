04952b0edea34706cbd0ffcf5a8a49916a6d85e0
Carmine Choi <carmine.choi@gmail.com>
2023-12-15 16:32:12 -0800

    Time: 50 ms (57.14%) | Memory: 18.7 MB (47.53%) - LeetSync

---
+++ 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def arrangeWords(self, text: str) -> str:
+        words = text.split(" ")
+        words.sort(key=lambda word: len(word))
+        
+        result = " ".join(words)
+        result = result.lower().capitalize()
+
+        return result
